cfbfd54c915e8fb2289d2c915c56abc27d0bab8b
JHJO <war2290@gmail.com>
2024-01-26 17:56:34 +0900

    forwardJSON

--- src/main/java/com/mtcoding/conbasic/controller/HttpBodyController.java
+++ src/main/java/com/mtcoding/conbasic/controller/HttpBodyController.java
@@ -1,6 +1,8 @@
 package com.mtcoding.conbasic.controller;
 
+import com.mtcoding.conbasic.dto.BoardReqDto;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +16,10 @@ public class HttpBodyController {
     @PostMapping("/data/body/v2")
     public String bodyV2(@RequestParam("title") String title, @RequestParam("content") String content) {
         return "받은 값 : " + title + ", " + content;
+    }
+
+    @PostMapping("/data/body/v3")
+    public String bodyV3(@RequestBody BoardReqDto boardReqDto) {
+        return "받은 값 : " + boardReqDto.getTitle() + ", " + boardReqDto.getContent();
     }
 }
